b69440b05032a28118832ecc9d84b7d415f0ffd9
Mateusz Gałganek <mateusz.maciej.galganek@gmail.com>
2014-10-23 19:55:31 +0200

    Dodano nowy widok

--- simple_auth/urls.py
+++ simple_auth/urls.py
@@ -5,8 +5,10 @@ admin.autodiscover()
 
 urlpatterns = patterns(
     '',
-    url(r'^login/', 'simple_auth.views.login_view'),
-    url(r'^auth/', 'simple_auth.views.auth_and_login'),
-    url(r'^register/', 'simple_auth.views.register'),
+    url(r'^login/$', 'simple_auth.views.login_view'),
+    url(r'^logout/$', 'simple_auth.views.logout_view'),
+    url(r'^auth/$', 'simple_auth.views.auth_and_login'),
+    url(r'^register/$', 'simple_auth.views.register'),
     url(r'^$', 'simple_auth.views.secured'),
+    url(r'^language/(?P<lang>[a-z\-]+)/$', 'simple_auth.views.language'),
 )
--- simple_auth/views.py
+++ simple_auth/views.py
@@ -1,14 +1,26 @@
 from django.core.context_processors import csrf
 from django.shortcuts import render_to_response, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.http import HttpResponse
 
 
 def login_view(request):
-    c = {}
-    c.update(csrf(request))
-    return render_to_response('login.html', c)
+    language = 'en'
+    session_language = 'en'
+
+    context = {}
+    context.update(csrf(request))
+    if 'lang' in request.COOKIES:
+        lang = request.COOKIES['lang']
+        context.update({'language': lang})
+
+    if 'lang' in request.session:
+        session_lang = request.session['lang']
+        context.update({'session_language': session_lang})
+
+    return render_to_response('login.html', context)
 
 
 def auth_and_login(request, onsuccess='/', onfail='/login/'):
@@ -50,4 +62,30 @@ def register(request):
 
 @login_required(login_url='/login/')
 def secured(request):
-    return render_to_response("profile.html")
+    lang = 'en'
+    session_lang = 'en'
+    context = {}
+
+    if 'lang' in request.COOKIES:
+        lang = request.COOKIES['lang']
+
+    if 'lang' in request.session:
+        session_lang = request.session['lang']
+
+    return HttpResponse(
+        'Access OK, Cookie language is {}, Session Language is {}'
+        .format(lang, session_lang)
+    )
+
+
+def logout_view(request):
+    logout(request)
+    return redirect("/login/")
+
+
+def language(request, lang='en'):
+    response = HttpResponse('Setting language to {}'.format(lang))
+    response.set_cookie('lang', lang)
+    request.session['lang'] = lang
+
+    return response
