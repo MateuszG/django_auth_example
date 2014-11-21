11df3d1217fa203bfd91a1280efac4aa651dced7
Mateusz Gałganek <mateusz.maciej.galganek@gmail.com>
2014-11-21 21:44:51 +0100

    Added language path

--- simple_auth/urls.py
+++ simple_auth/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-
+# from django.contrib.admin import site
 from django.contrib import admin
 admin.autodiscover()
 
@@ -11,4 +11,6 @@ urlpatterns = patterns(
     url(r'^register/$', 'simple_auth.views.register'),
     url(r'^$', 'simple_auth.views.secured'),
     url(r'^language/(?P<lang>[a-z\-]+)/$', 'simple_auth.views.language'),
+    url(r'^language/$', 'simple_auth.views.language'),
+    # url(r'^admin/', include(site.urls)),
 )
